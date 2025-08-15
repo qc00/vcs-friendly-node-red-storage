8321ecf0c9a85e837caf098cd50d5f073cf7a942
QC <1662412+qc00@users.noreply.github.com>
2025-08-15 20:37:32 +0100

    Proper auto add to git handling and other fixes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ B: Node coordinates
 
 import * as Path from "path";
 import * as fs from "fs/promises";
-import type { Module } from "module";
 import type { Node } from "node-red";
 import type { Log } from "@node-red/util";
 
@@ -16,8 +15,9 @@ const nodeRedModules = function fishPath() {
   return fn.substring(0, fn.length - needle.length) + "node_modules";
 }();
 const log: Log = require(Path.join(nodeRedModules, "@node-red/util")).log;
-const stageFile = require(Path.join(nodeRedModules, "@node-red/runtime/lib/storage/localfilesystem/projects/git")).stageFile;
-if (!log || !stageFile) {
+const projects = require(Path.join(nodeRedModules, "@node-red/runtime/lib/storage/localfilesystem/projects"));
+const util: UtilModule = require(Path.join(nodeRedModules, "@node-red/runtime/lib/storage/localfilesystem/util.js"));
+if (!log || !util) {
   throw new Error("Failed to extract modules from node-red runtime");
 }
 
@@ -31,45 +31,134 @@ interface NodeWithOffload extends Node {
   y?: number;
 }
 
-//
-type WriteFile = (path: string, content: any, backupPath: string) => Promise<void>;
-let originalWriteFile: WriteFile;
+interface UtilModule {
+  parseJSON(text: string): any;
+  writeFile(path: string, content: string | NodeWithOffload[], backupPath: string): Promise<void>;
+  readFile(path: string, backupPath: string, emptyResponse: any, type: string): Promise<any>;
+}
 
+// @node-red/runtime/lib/storage/localfilesystem/projects/Project.js
+interface Project {
+  stageFile(files: any): Promise<void>;
+  getFlowFile(): string | null;
+}
+
+function getOriginal<T extends Function>(fn: T): T {
+  return (fn as any)?._vcsFriendlyOriginal || fn;
+}
+
+//
+const saveSerializing: { [path: string]: Promise<unknown> } = {};
+let addToGit: string[] = [];
+
+const origSaveFlows = getOriginal(projects?.saveFlows);
+if (origSaveFlows) {
+  projects.saveFlows = function vcsFriendlySaveFlows(flows: any) {
+    const original = () => origSaveFlows.apply(this, arguments);
+    const active: Project = projects.getActiveProject();
+    const flowFile = active?.getFlowFile();
+    if (!flowFile) return original();
+
+    async function body() {
+      // Patch auto-commit files
+      const origStageFile = active.stageFile;
+      if (origStageFile) {
+        addToGit.length = 0;
+        active.stageFile = function (files: any) {
+          if (Array.isArray(files) && files.includes(flowFile)) {
+            files.push(...addToGit);
+          }
+          return origStageFile.apply(this, arguments);
+        };
+      }
+
+      try {
+        return await withPatchedStringify(flows, original);
+      } finally {
+        active.stageFile = origStageFile;
+        addToGit.length = 0;
+      }
+    }
+
+    // Simple serializing to protect addToGit
+    const prev = saveSerializing[flowFile];
+    const out = prev ? prev.then(body) : body();
+    saveSerializing[flowFile] = out.then(_=>1, _=>0);
+    return out;
+  };
+  (projects.saveFlows as any)._vcsFriendlyOriginal = origSaveFlows;
+
+  // Patch JSON.stringify to avoid having to immediately deserialize
+  function withPatchedStringify(flows: any, callback: () => any) {
+    const origStringify = JSON.stringify;
+    JSON.stringify = function replacementStringify(obj: any) {
+      return Object.is(obj, flows) ? structuredClone(obj) : origStringify.apply(this, arguments);
+    };
+
+    try {
+      // At the time of writing, the origSaveFlows does not use await meaning stringify is called synchronously. Thus,
+      // we restore stringify synchronously to avoid an unpredictable chain of origStringify.
+      return callback();
+    } finally {
+      JSON.stringify = origStringify;
+    }
+  }
+}
+
+//
 const rules: {[nodeType: string]: {[prop: string]: string}} = {
   "function": {"func": "js", "initialize": "js"},
   "ui_template": {"format": "htm"},
 };
 
-const writeFile: WriteFile = async (path, content, backupPath) => {
+function shouldOffload(node: NodeWithOffload, prop: string, val: any) {
+  if (!val) return false;
+  if (typeof val !== 'string') {
+    log.warn(`${prop} in ${node.type} is not a string`);
+    return false;
+  }
+  if (!val.includes("\n") && val.length < 200) return false;
+  return true;
+}
+
+const originalWriteFile = getOriginal(util.writeFile);
+
+util.writeFile = async (path, content, backupPath) => {
   if (path.endsWith(flowSuffix)) {
+    log.info(`Processing flow file [${path}]`);
     const dir = Path.dirname(path);
     const allPromises: Promise<any>[] = [];
-    const addToGit: string[] = [];
     const coord = [];
+
+    if (typeof content === 'string') {
+      content = util.parseJSON(content);
+    }
 
     for (const node of content as NodeWithOffload[]) {
       // A:
+      delete node._vcsOffload;
+
       const rule = rules[node.type];
       if (rule) {
+        log.debug(`Processing ${node.type} node [${node.id}]`);
         for (const [prop, ext] of Object.entries(rule)) {
           const p = Path.join(dir, `${node.id}.${prop}.${ext}`);
-          const del = fs.unlink(p);
-          allPromises.push(del);
-          addToGit.push(p);
-
           const val = node[prop];
-          if (!val) continue;
-          if (typeof val !== 'string') {
-            log.warn(`${prop} in ${node.type} is not a string`);
-            continue;
+          if (shouldOffload(node, prop, val)) {
+            allPromises.push(fs.writeFile(p, val));
+            addToGit.push(p);
+            (node._vcsOffload || (node._vcsOffload = [])).push(`${prop}.${ext}`);
+            delete node[prop];
+          } else {
+            allPromises.push(fs.unlink(p)
+              .then(() => addToGit.push(p))
+              .catch(_ => void 0));
           }
-          allPromises[allPromises.length - 1] = del.then(() => fs.writeFile(p, val));
-          (node._vcsOffload || (node._vcsOffload = [])).push(`${prop}.${ext}`);
         }
       }
 
       // B:
-      if ("x" in node && "y" in node) {
+      if (node.hasOwnProperty("x") && node.hasOwnProperty("y")) {
         coord.push(node.id, node.x, node.y);
         delete node.x;
         delete node.y;
@@ -78,25 +167,24 @@ const writeFile: WriteFile = async (path, content, backupPath) => {
 
     allPromises.push(fs.writeFile(Path.join(dir, COORD_FILE), JSON.stringify(coord))); // Not added to git
     await Promise.all(allPromises);
-    await stageFile(dir, addToGit);
+    content = JSON.stringify(content, null, 4);
   }
   return originalWriteFile(path, content, backupPath);
 };
 
+(util.writeFile as any)._vcsFriendlyOriginal = originalWriteFile;
+
 //
-let parseJSON: (text: string) => any;
+const originalReadFile = getOriginal(util.readFile);
 
-type ReadFile<R> = (path: string, backupPath: string, emptyResponse: R, type: string) => Promise<R>;
-let originalReadFile: ReadFile<any>;
-
-const readFile: ReadFile<any> = async (path, backupPath, emptyResponse, type) => {
+util.readFile = async (path, backupPath, emptyResponse, type) => {
   const data = await originalReadFile(path, backupPath, emptyResponse, type);
-  if (Object.is(data, emptyResponse) || type !== 'flow') return data;
-  if (!path.endsWith(flowSuffix)) {
-    throw new Error(`Unsupported flow file [${path}]`);
+  if (Object.is(data, emptyResponse) || !path.endsWith(flowSuffix)) {
+    return data;
   }
+
   const dir = Path.dirname(path);
-  const coordPromise = fs.readFile(Path.join(dir, COORD_FILE), 'utf8').then(parseJSON);
+  const coordPromise = fs.readFile(Path.join(dir, COORD_FILE), 'utf8').then(util.parseJSON);
 
   const allPromises: Promise<any>[] = [];
   const lookup: {[id: string]: NodeWithOffload} = {};
@@ -110,11 +198,11 @@ const readFile: ReadFile<any> = async (path, backupPath, emptyResponse, type) =>
 
     if (node._vcsOffload) {
       for (const offload of node._vcsOffload) {
-        if (Path.isAbsolute(offload) || offload.includes("..")) {
+        if (offload.includes("..")) {
           throw new Error(`Invalid offload path [${offload}] in node ${node.id}`);
         }
         const prop = offload.substring(0, offload.lastIndexOf('.'));
-        const promise = fs.readFile(Path.join(dir, offload), 'utf8').then(data => node[prop] = data);
+        const promise = fs.readFile(Path.join(dir, `${node.id}.${offload}`), 'utf8').then(data => node[prop] = data);
         allPromises.push(promise);
       };
 
@@ -146,30 +234,4 @@ const readFile: ReadFile<any> = async (path, backupPath, emptyResponse, type) =>
   return data;
 };
 
-//
-function withModuleUncached<R>(file: string, handler: (real: Module, path: string) => R): R {
-  const path = Path.join(nodeRedModules, "@node-red/runtime/lib/storage/localfilesystem/", file);
-  const origModule = require(path);
-  const fromCache = require.cache[path];
-  if (!Object.is(fromCache.exports, origModule)) {
-    throw new Error(`Module ${path} is not cached as expected`);
-  }
-  delete require.cache[path];
-  try {
-    return handler(fromCache, path);
-  } finally {
-    require.cache[path] = fromCache;
-  }
-}
-
-module.exports = withModuleUncached("index.js", (_, lfsPath) => {
-  withModuleUncached("util.js", (origUtilModule, path) => {
-    originalReadFile = origUtilModule.exports.readFile;
-    originalWriteFile = origUtilModule.exports.writeFile;
-    require.cache[path] = {
-      ...origUtilModule,
-      exports: { ...origUtilModule.exports, readFile, writeFile },
-    };
-  });
-  return require(lfsPath);
-});
+(util.readFile as any)._vcsFriendlyOriginal = originalReadFile;
